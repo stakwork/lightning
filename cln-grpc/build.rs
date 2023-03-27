d5b666557b9563eddbf27d1c5d43da4589fdaf3f
Evan Feenstra <evanfeenstra@gmail.com>
2023-03-27 11:02:05 -0700

    serialize to geninfo response

--- cln-grpc/build.rs
+++ cln-grpc/build.rs
@@ -2,6 +2,12 @@ fn main() {
     let builder = tonic_build::configure();
     builder
         .protoc_arg("--experimental_allow_proto3_optional")
+        .type_attribute(".cln.GetinfoResponse", "#[derive(serde::Serialize)]")
+        .type_attribute(".cln.GetinfoOur_features", "#[derive(serde::Serialize)]")
+        .type_attribute(".cln.GetinfoAddress", "#[derive(serde::Serialize)]")
+        .type_attribute(".cln.GetinfoAddressType", "#[derive(serde::Serialize)]")
+        .type_attribute(".cln.GetinfoBinding", "#[derive(serde::Serialize)]")
+        .type_attribute(".cln.GetinfoBindingType", "#[derive(serde::Serialize)]")
         .compile(&["proto/node.proto"], &["proto"])
         .unwrap();
 }
